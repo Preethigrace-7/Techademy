9e66eca1bff73d3cc48b5aeb852b085c478c87ca
Preethi grace <81759829+Preethigrace-7@users.noreply.github.com>
2023-07-29 14:18:40 +0530

    Update DM_alloc.cpp

--- DM_alloc.cpp
+++ DM_alloc.cpp
@@ -3,6 +3,34 @@
 using namespace std;
 
 int main(){
+    int *p=NULL;
+    p=new int;
+    if(!p){
+        cout<<"memoery is not allocated\n";
+    }
+    else{
+        *p=76;
+        cout<<"value of p ="<< *p <<endl;
+    }
+    float *r=new float(54.23);
+    cout<<"value of r="<< *r <<endl;
+    
+    int n=5;
+    int *w=new int[n];
+    if(!w){
+        cout<<"memory is not allocated\n";
+    }
+    else{
+        for(int i=0;i<n;i++){
+            w[i]=i+1;
+        }
+        cout<<"stored values are=";
+        for(int i=0;i<n;i++){
+            cout<<w[i]<<" ";
+            }
+            cout<<"\n";
+    }
+   
     string *s=NULL;
     s=new string;
     if(!s){
@@ -12,6 +40,10 @@ int main(){
         *s="Techademy";
         cout<<"S contains ="<< *s <<endl;
     }
+    delete p;
+    delete r;
+    delete []w;
+    return 0;
     delete s;//is not memory leakage will occur
     return 0;
 }
